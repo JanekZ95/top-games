adac0d9cf58a1e9f3ff6884b40af71e494936b3b
JohnyZ <94840148+JanekZ95@users.noreply.github.com>
2022-06-21 13:27:11 +0200

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -5,6 +5,10 @@ menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 })
 
+document.querySelectorAll(".border").forEach(n => n.addEventListener("click", () => {
+    menu.classList.remove("active");
+}))
+
 window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
